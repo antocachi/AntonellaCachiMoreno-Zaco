63a14850c6abca82d53d9c245ce6ffd7f468f655
Antonella Cachi <antonellacachimoreno@gmail.com>
2022-06-18 21:20:38 -0300

    PrimeraEntregaPF
    PrimeraEntregaPF

---
+++ Java/main.js
@@ -0,0 +1,6 @@
+const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
+if(edadDelUsuario >= 18){
+    alert("Eres mayor de edad, puede ingresar");
+}else{
+    alert("Lo siento eres menor de edad");
+}
