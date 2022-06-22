de4b507f8878910a6a1c98a768e46ce97ad79f38
Antonella Cachi <antonellacachimoreno@gmail.com>
2022-06-22 20:43:36 -0300

    Add Incorporar Eventos

--- Java/main.js
+++ Java/main.js
@@ -2,5 +2,5 @@ const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
 if(edadDelUsuario >= 18){
     alert("Eres mayor de edad, puede ingresar");
 }else{
-    alert("Lo siento eres menor de edad");
+    alert("Eres menor de edad, ingresa con un mayor");
 }
